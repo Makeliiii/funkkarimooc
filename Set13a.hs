68b87aed365a36d0e22d3640114f0b7484056701
Makeliiii <joel.maenpaa@ibm.com>
2021-11-06 23:01:34 +0200

    monad god, no fucking clue what i'm doing

--- Set13a.hs
+++ Set13a.hs
@@ -1,15 +1,12 @@
 module Set13a where
 
-import Mooc.Todo
-
 import Control.Monad
 import Control.Monad.Trans.State
 import Data.Char
 import Data.List
 import qualified Data.Map as Map
-
 import Examples.Bank
-
+import Mooc.Todo
 
 ------------------------------------------------------------------------------
 -- Ex 1: Your task is to help implement the function readName that
@@ -27,37 +24,61 @@ import Examples.Bank
 -- works correctly.
 
 (?>) :: Maybe a -> (a -> Maybe b) -> Maybe b
-Nothing ?> _ = Nothing   -- In case of failure, propagate failure
-Just x  ?> f = f x       -- In case of sucess, run the next computation
+Nothing ?> _ = Nothing -- In case of failure, propagate failure
+Just x ?> f = f x -- In case of sucess, run the next computation
 
 -- DO NOT touch this definition!
-readNames :: String -> Maybe (String,String)
+readNames :: String -> Maybe (String, String)
 readNames s =
   split s
-  ?>
-  checkNumber
-  ?>
-  checkCapitals
+    ?> checkNumber
+    ?> checkCapitals
 
 -- split should split a string into two words. If the input doesn't
 -- contain a space, Nothing should be returned
 --
 -- (NB! There are obviously other corner cases like the inputs " " and
 -- "a b c", but you don't need to worry about those here)
-split :: String -> Maybe (String,String)
-split = todo
+split :: String -> Maybe (String, String)
+split s = if ' ' `elem` s
+          then Just (apustaja s [])
+          else Nothing
+  where
+    apustaja (x : xs) f = if x == ' '
+                            then (reverse f, xs)
+                            else apustaja xs (x : f)
 
 -- checkNumber should take a pair of two strings and return them
 -- unchanged if they don't contain numbers. Otherwise Nothing is
 -- returned.
 checkNumber :: (String, String) -> Maybe (String, String)
-checkNumber = todo
+checkNumber (for, sur) = case (apustaja for [], apustaja sur []) of (Just x, Just y) -> Just (reverse x, reverse y)
+                                                                    _                -> Nothing
+  where
+    apustaja :: String -> String -> Maybe String
+    apustaja [] name = Just name
+    apustaja (x:xs) name
+      | x == '0' = Nothing
+      | x == '1' = Nothing
+      | x == '2' = Nothing
+      | x == '3' = Nothing
+      | x == '4' = Nothing
+      | x == '5' = Nothing
+      | x == '6' = Nothing
+      | x == '7' = Nothing
+      | x == '8' = Nothing
+      | x == '9' = Nothing
+      | otherwise = apustaja xs (x:name)
 
 -- checkCapitals should take a pair of two strings and return them
 -- unchanged if both start with a capital letter. Otherwise Nothing is
 -- returned.
 checkCapitals :: (String, String) -> Maybe (String, String)
-checkCapitals (for,sur) = todo
+checkCapitals (for, sur) = apustaja for sur
+  where
+    apustaja xs ys = if toUpper (head xs) == head xs && toUpper (head ys) == head ys
+                     then Just (for,sur)
+                     else Nothing
 
 ------------------------------------------------------------------------------
 -- Ex 2: Given a list of players and their scores (as [(String,Int)]),
@@ -83,8 +104,11 @@ checkCapitals (for,sur) = todo
 --   winner [("a",1),("b",1)] "a" "b"
 --     ==> Just "a"
 
-winner :: [(String,Int)] -> String -> String -> Maybe String
-winner scores player1 player2 = todo
+winner :: [(String, Int)] -> String -> String -> Maybe String
+winner scores player1 player2 = case (lookup player1 scores, lookup player2 scores) of (Just s1, Just s2) -> if s1 >= s2
+                                                                                                             then Just player1
+                                                                                                             else Just player2
+                                                                                       _                  -> Nothing
 
 ------------------------------------------------------------------------------
 -- Ex 3: given a list of indices and a list of values, return the sum
@@ -101,8 +125,20 @@ winner scores player1 player2 = todo
 --  selectSum [0..10] [4,6,9,20]
 --    Nothing
 
+safeIndex :: Num a => [a] -> Int -> Maybe a
+safeIndex xs i = if i > length xs - 1 || i < 0
+                 then Nothing
+                 else Just (xs !! i)
+
 selectSum :: Num a => [a] -> [Int] -> Maybe a
-selectSum xs is = todo
+selectSum xs is = abuapustaja
+  where
+    apustaja xs is = safeIndex xs <$> is
+    abustaja = apustaja xs is >>= \ys -> return ys
+    abuapustaja = apuapustaja 0 abustaja
+    apuapustaja sum [] = Just sum
+    apuapustaja sum (x:xs) = case x of Nothing  -> Nothing
+                                       (Just x) -> apuapustaja (x+sum) xs
 
 ------------------------------------------------------------------------------
 -- Ex 4: Here is the Logger monad from the course material. Implement
@@ -126,8 +162,9 @@ instance Functor Logger where
 
 instance Monad Logger where
   return x = Logger [] x
-  Logger la a >>= f = Logger (la++lb) b
-    where Logger lb b = f a
+  Logger la a >>= f = Logger (la ++ lb) b
+    where
+      Logger lb b = f a
 
 -- This is an Applicative instance that works for any monad, you
 -- can just ignore it for now. We'll get back to Applicative later.
@@ -136,7 +173,9 @@ instance Applicative Logger where
   (<*>) = ap
 
 countAndLog :: Show a => (a -> Bool) -> [a] -> Logger Int
-countAndLog = todo
+countAndLog f xs = Logger (logged f xs) (length $ logged f xs)
+  where
+    logged f xs = map show $ filter f xs
 
 ------------------------------------------------------------------------------
 -- Ex 5: You can find the Bank and BankOp code from the course
@@ -150,10 +189,17 @@ countAndLog = todo
 -- from Data.Map are available under the prefix Map.
 
 exampleBank :: Bank
-exampleBank = (Bank (Map.fromList [("harry",10),("cedric",7),("ginny",1)]))
+exampleBank = (Bank (Map.fromList [("harry", 10), ("cedric", 7), ("ginny", 1)]))
+
+bankLookup :: String -> Bank -> Maybe Int
+bankLookup accountName (Bank bank) = Map.lookup accountName bank
 
 balance :: String -> BankOp Int
-balance accountName = todo
+balance accountName = BankOp apustaja
+  where
+    apustaja bank = case bankLookup accountName bank of Just x  -> (x, bank)
+                                                        Nothing -> (0, bank)
+
 
 ------------------------------------------------------------------------------
 -- Ex 6: Using the operations balance, withdrawOp and depositOp, and
@@ -171,7 +217,7 @@ balance accountName = todo
 --     ==> ((),Bank (fromList [("cedric",7),("ginny",1),("harry",10)]))
 
 rob :: String -> String -> BankOp ()
-rob from to = todo
+rob from to = balance from +> withdrawOp from +> depositOp to
 
 ------------------------------------------------------------------------------
 -- Ex 7: using the State monad, write the operation `update` that first
@@ -183,7 +229,7 @@ rob from to = todo
 --    ==> ((),7)
 
 update :: State Int ()
-update = todo
+update = state (\s -> ((), s * 2 + 1))
 
 ------------------------------------------------------------------------------
 -- Ex 8: Checking that parentheses are balanced with the State monad.
@@ -211,11 +257,15 @@ update = todo
 --   parensMatch "(()))("      ==> False
 
 paren :: Char -> State Int ()
-paren = todo
+paren x = case x of '(' -> state (\s -> ((), case s of (-1) -> negate 1
+                                                       _    -> s + 1))
+                    ')' -> state (\s -> ((), case s of (-1) -> negate 1
+                                                       _    -> s - 1))
 
 parensMatch :: String -> Bool
 parensMatch s = count == 0
-  where (_,count) = runState (mapM_ paren s) 0
+  where
+    (_, count) = runState (mapM_ paren s) 0
 
 ------------------------------------------------------------------------------
 -- Ex 9: using a state of type [(a,Int)] we can keep track of the
@@ -241,8 +291,12 @@ parensMatch s = count == 0
 --
 -- PS. The order of the list of pairs doesn't matter
 
-count :: Eq a => a -> State [(a,Int)] ()
-count x = todo
+count :: Eq a => a -> State [(a, Int)] ()
+count x = state (\s -> ((), case lookup x s of Nothing -> (x, 1) : s
+                                               Just y  -> apustaja x s []))
+  where
+    apustaja _ [] _ = []
+    apustaja x (s:ss) rest = if x == fst s then (x, snd s + 1) : ss ++ rest else apustaja x ss (s:rest)
 
 ------------------------------------------------------------------------------
 -- Ex 10: Implement the operation occurrences, which
@@ -263,5 +317,9 @@ count x = todo
 --  runState (occurrences [4,7]) [(2,1),(3,1)]
 --    ==> (4,[(2,1),(3,1),(4,1),(7,1)])
 
-occurrences :: (Eq a) => [a] -> State [(a,Int)] Int
+occurrences :: (Eq a) => [a] -> State [(a, Int)] Int
 occurrences xs = todo
+  where
+    apustaja [] newState = newState
+    apustaja (x:xs) newState = apustaja xs (count x)
+    apuapustaja = apustaja xs (state (const ((), [])))
