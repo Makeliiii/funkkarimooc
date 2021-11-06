39a336b510b091dbe62cefe2e7c8332c4e98852a
Makeliiii <joel.maenpaa@ibm.com>
2021-11-06 23:55:40 +0200

    asd

--- Set13b.hs
+++ Set13b.hs
@@ -40,7 +40,7 @@ test = do
   return (x<10)
 
 ifM :: Monad m => m Bool -> m a -> m a -> m a
-ifM opBool opThen opElse = todo
+ifM opBool opThen opElse = opBool >>= (\b -> if b then opThen else opElse)
 
 ------------------------------------------------------------------------------
 -- Ex 2: the standard library function Control.Monad.mapM defines a
